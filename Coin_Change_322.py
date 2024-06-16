92f27a793d6c8a0e1cfa14aeb185afeca2daff9d
Frankie Pasztor <frankie.pasztor@gmail.com>
2024-06-16 11:46:29 -0400

    2024-06-14

--- Coin_Change_322.py
+++ Coin_Change_322.py
@@ -3,7 +3,7 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [amount + 1] * (amount + 1) # [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
         dp[0] = 0 # [0, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
-        # first iteration of the loop is shown with the comments
+        # (first iteration of the loop is shown with the comments)
         for a in range(1, amount + 1): # 1 or [0, (12), 12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
             for c in coins: # [(1), 2, 5]
                 if a - c >= 0: # 1 - 1 >= 0 (true)
